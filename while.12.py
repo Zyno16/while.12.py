658a7b7db6670f2da231d1ddb2c3dbb613feae80
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-02-25 12:57:57 +0000

    Add files via upload

---
+++ while.12.py
@@ -0,0 +1,16 @@
+marakech =1000000
+
+akadir =500000
+nbr_age = 0
+while marakech > akadir:
+
+     marakech = marakech + 50000
+     akadir = akadir*1.08
+
+     nbr_age = nbr_age + 1
+print(nbr_age)
+
+
+
+
+
